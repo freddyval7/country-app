4df228a577c5f6f774bfd05691dc94aecab6a01e
Freddy Tomada <freddytomada7@gmail.com>
2026-02-17 18:10:49 -0400

    add search input and pages

--- src/app/country/pages/by-country-page/by-country-page.ts
+++ src/app/country/pages/by-country-page/by-country-page.ts
@@ -3,8 +3,8 @@ import { SearchInput } from '../../components/search-input/search-input';
 import { CountryList } from '../../components/country-list/country-list';
 
 @Component({
-  selector: 'app-by-capital-page',
+  selector: 'app-by-country-page',
   imports: [SearchInput, CountryList],
-  templateUrl: './by-capital-page.html',
+  templateUrl: './by-country-page.html',
 })
-export class ByCapitalPage {}
+export class ByCountryPage {}
