30f73d19057ffce6fd0ce319291d41c4747b7893
Freddy Tomada <freddytomada7@gmail.com>
2026-02-17 19:03:45 -0400

    add service schema and interface for country rest

--- src/app/country/pages/by-capital-page/by-capital-page.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SearchInput } from '../../components/search-input/search-input';
 import { CountryList } from '../../components/country-list/country-list';
+import { CountryService } from '../../services/country';
 
 @Component({
   selector: 'app-by-capital-page',
   imports: [SearchInput, CountryList],
   templateUrl: './by-capital-page.html',
 })
-export class ByCapitalPage {}
+export class ByCapitalPage {
+  countryService = inject(CountryService);
+
+  onSearch(query: string) {
+    this.countryService.searchByCapital(query).subscribe((resp) => console.log(resp));
+  }
+}
