6a082fbb2c955f60dc4f0e6cd4f6ed41bbb88146
Freddy Tomada <freddytomada7@gmail.com>
2026-02-17 13:06:17 -0400

    add layout and buttons

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,19 +1,22 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPage } from './pages/by-capital-page/by-capital-page';
+import { CountryLayout } from './layouts/CountryLayout/CountryLayout';
 
 export const countryRoutes: Routes = [
   {
     path: '',
-    component: ByCapitalPage,
+    component: CountryLayout,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPage,
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      },
+    ],
   },
-  // {
-  //   path: 'country',
-  //   // ??
-  // },
-  // {
-  //   path: '**',
-  //   redirectTo: '',
-  // },
 ];
 
 export default countryRoutes;
