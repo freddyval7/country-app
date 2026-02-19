c4f624042481262ae847485b7fe06a162fb81ccb
Freddy Tomada <freddytomada7@gmail.com>
2026-02-19 19:02:40 -0400

    add language and currencies in country interface

--- src/app/country/interfaces/country.interface.ts
+++ src/app/country/interfaces/country.interface.ts
@@ -1,3 +1,14 @@
+export interface Currency {
+  code: string;
+  name: string;
+  symbol: string;
+}
+
+export interface Language {
+  code: string;
+  name: string;
+}
+
 export interface Country {
   cca2: string;
   flag: string;
@@ -5,4 +16,8 @@ export interface Country {
   name: string;
   capital: string;
   population: number;
+  currencies: Currency[];
+  languages: Language[]; // Array de idiomas
+  mainCurrency?: Currency;
+  mainLanguage?: string; // Opcional: el primer idioma como string
 }
--- src/app/country/mappers/country.mapper.ts
+++ src/app/country/mappers/country.mapper.ts
@@ -1,8 +1,23 @@
-import type { Country } from '../interfaces/country.interface';
+import type { Country, Currency, Language } from '../interfaces/country.interface';
 import type { RESTCountry } from '../interfaces/rest-countries.interfaces';
 
 export class CountryMapper {
   static mapRESTCountryToCountry(item: RESTCountry): Country {
+    // Mapear monedas
+    const currencies: Currency[] = Object.entries(item.currencies || {}).map(
+      ([code, currency]) => ({
+        code,
+        name: currency.name,
+        symbol: currency.symbol,
+      }),
+    );
+
+    // Mapear idiomas
+    const languages: Language[] = Object.entries(item.languages || {}).map(([code, name]) => ({
+      code,
+      name,
+    }));
+
     return {
       cca2: item.cca2,
       flag: item.flag,
@@ -10,6 +25,10 @@ export class CountryMapper {
       name: item.translations['spa'].common ?? 'No Spanish Name',
       capital: item.capital.join(', '),
       population: item.population,
+      currencies: currencies,
+      languages: languages,
+      mainCurrency: currencies[0], // Asignar la primera moneda como principal
+      mainLanguage: languages[0]?.name, // Asignar el primer idioma como principal
     };
   }
 
