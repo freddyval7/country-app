3618fe9f08d1b0026e11bd095cb8fac0741615f3
Freddy Tomada <freddytomada7@gmail.com>
2026-02-20 21:50:50 -0400

    add persistant to by-region

--- src/app/country/pages/by-region-page/by-region-page.ts
+++ src/app/country/pages/by-region-page/by-region-page.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, linkedSignal, signal } from '@angular/core';
 import { CountryList } from '../../components/country-list/country-list';
 import { CountryService } from '../../services/country';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { of } from 'rxjs';
 import { Region } from '../../interfaces/region.interface';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-by-region-page',
@@ -11,14 +12,25 @@ import { Region } from '../../interfaces/region.interface';
   templateUrl: './by-region-page.html',
 })
 export class ByRegionPage {
+  activatedRoute = inject(ActivatedRoute);
+  route = inject(Router);
+
+  queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
+
   public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania', 'Antarctic'];
   countryService = inject(CountryService);
-  selectedRegion = signal<Region | null>(null);
+  selectedRegion = linkedSignal<Region | null>(() => this.queryParam as Region);
 
   countryResource = rxResource({
     params: () => ({ region: this.selectedRegion() }),
     stream: ({ params }) => {
       if (!params.region) return of([]);
+
+      this.route.navigate(['/country/by-region'], {
+        queryParams: {
+          query: params.region,
+        },
+      });
 
       return this.countryService.searchByRegion(params.region);
     },
